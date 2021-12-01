798eab1f00b7e6cec397ec154a11211018888f34
Rasheed Mohammed <rasheednour@gmail.com>
2021-12-01 21:21:30 +0300

    Set-up exercises table

--- exercise-frontend/src/Pages/HomePage.js
+++ exercise-frontend/src/Pages/HomePage.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom';
-
+import ExerciseList from '../Components/ExerciseList';
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function HomePage() {
+
+  const [exercises, setExercises ] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="h1">
           Exercise Tracker!
         </h1>
-        <h2>
+        <h2> List of Exercises 
+            <ExerciseList exercises={exercises}></ExerciseList>
             <Link to="/create-exercise" className="button1">
             Create exercise
             </Link>
