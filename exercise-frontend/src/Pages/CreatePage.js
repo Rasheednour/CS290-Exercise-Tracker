b15234dd2d9433bd2c2eb88474b2bcfc709d25ec
Rasheed Mohammed <rasheednour@gmail.com>
2021-12-02 21:45:08 +0300

    Set-up initial (add exercise) page

--- exercise-frontend/src/Pages/CreatePage.js
+++ exercise-frontend/src/Pages/CreatePage.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import '../App.css';
+import { useState } from 'react';
+import { useHistory } from "react-router-dom";
 
+export const CreatePage = () => {
+  const [name, setName] = useState('');
+  const [reps, setReps] = useState('');
+  const [weight, setWeight] = useState('');
+  const [unit, setUnit] = useState('');
+  const [date, setDate] = useState('');
 
-function CreatePage() {
+  const addExercise = async () => {
+
+  }
+
   return (
     <div>
-        <h1 className="h1">
+        <h1>
           Create new exercise!
         </h1>
-        <table id="exercises">
+        <table>
         <thead>
             <tr>
                 <th>Name</th>
@@ -18,7 +29,55 @@ function CreatePage() {
                 <th>Date</th>
             </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>
+              <input
+                type="text"
+                placeholder="Enter exercise name here"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                placeholder="Enter number of reps here"
+                value={reps}
+                onChange={e => setReps(e.target.value)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                placeholder="Enter weight here"
+                value={weight}
+                onChange={e => setWeight(e.target.value)}
+              />
+            </td>
+            <td>
+              <input
+                type="text"
+                placeholder="Enter weight unit here"
+                value={unit}
+                onChange={e => setUnit(e.target.value)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                placeholder="Enter date here"
+                value={date}
+                onChange={e => setDate(e.target.value)}
+              />
+            </td>
+          </tr>
+        </tbody>
         </table>
+        <button
+        onClick={addExercise}>
+          Add Exercise
+          </button>
     </div>
   );
 }
